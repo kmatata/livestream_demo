bb511d68e5a9349e6ecd83f964f0ec94c6c0d2c2
kmatata <95537935+kmatata@users.noreply.github.com>
2022-10-29 06:59:20 +0300

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e)yt@cm4w^x$t*9s7y0kn08%&^bj(-nvzu1r_)=y8&0p#p&ts+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
